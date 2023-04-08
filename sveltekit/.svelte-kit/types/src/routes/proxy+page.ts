804dfb1d61f6eea8481adb03d600109d17993229
Richard Moore <richard.moore@yozobi.com>
2023-04-08 10:33:42 +0100

    check sveltekit generated types in to git to avoid error on first run of devserver

---
+++ sveltekit/.svelte-kit/types/src/routes/proxy+page.ts
@@ -0,0 +1,27 @@
+// @ts-nocheck
+import type { PageLoad } from "./$types";
+import { createClient } from "wundergraph/generated/client";
+import type { ReadPostsResponseData } from "wundergraph/generated/models";
+
+export const load = async ({ fetch, setHeaders }: Parameters<PageLoad>[0]) => {
+	const client = createClient({
+		baseURL: "http://127.0.0.1:9991",
+		customFetch: fetch,
+	});
+
+	const { data, error } = await client.query({
+		operationName: "ReadPosts",
+	});
+
+	setHeaders({
+		"Cache-Control":
+			error /* don't cache errored pages */ ||
+			import.meta.env.DEV /* don't cache when working on localhost */
+				? "no-cache"
+				: "max-age: 3600, public",
+		// max-age=3600 - refresh cache if the cached copy is older than 1hr (uses the Age header)
+		// public - tells CDNs to cache this page (not just browsers)
+	});
+
+	return data as ReadPostsResponseData;
+};
